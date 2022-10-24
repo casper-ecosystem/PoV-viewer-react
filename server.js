0c2cba10feb7f90725c4409dbc9c949e8aac9302
Dylan Ireland <dylan@casper.network>
2022-10-24 22:17:09 +0000

    Added server rules, SSL

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require("express")
 const app = express() // create express app
 var cors = require('cors')
 const { CasperClient, Contracts, CLPublicKey } = require('casper-js-sdk')
+const http = require('http')
+const https = require('https')
 const path = require("path")
+const fs = require('fs')
 
 const client = new CasperClient("http://35.180.42.211:7777/rpc") ///SHOULD PROBABLY BE DYNAMIC
 const contract = new Contracts.Contract(client)
@@ -11,6 +14,16 @@ contract.setContractHash("hash-1bbb4b8801e8409a4a10cd2d53d6dfaea89cb97770e716946
 app.use(cors())
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'build')))
+
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/proofofvictory.com/privkey.pem', 'utf8')
+const certificate = fs.readFileSync('/etc/letsencrypt/live/proofofvictory.com/cert.pem', 'utf8')
+const ca = fs.readFileSync('/etc/letsencrypt/live/proofofvictory.com/chain.pem', 'utf8')
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+}
 
 async function getPoVs(pubKey) {
     return contract.queryContractDictionary("owned_tokens", CLPublicKey.fromHex(pubKey).toAccountHashStr().substring(13, CLPublicKey.fromHex(pubKey).toAccountHashStr().length))
@@ -61,6 +74,13 @@ app.get("/metadata", (req, res) => {
 });
 
 
-app.listen(443, () => {
-  console.log("server started on port 443")
+const httpServer = http.createServer(app)
+const httpsServer = https.createServer(credentials, app)
+
+httpServer.listen(3001, () => {
+	console.log('HTTP Server running on port 80')
+});
+
+httpsServer.listen(3000, () => {
+	console.log('HTTPS Server running on port 443')
 });
